76a15b9277f46cb6139cf6cd018c3624795cfbb8
Nate <pasiebu@gmail.com>
2026-07-03 20:17:22 +0000

    feat(db): add superadmin role and logged_in/changed_user_role audit actions

---
+++ server/prisma/migrations/20260703201707_add_superadmin_and_audit_actions/migration.sql
@@ -0,0 +1,13 @@
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "ActivityAction" ADD VALUE 'logged_in';
+ALTER TYPE "ActivityAction" ADD VALUE 'changed_user_role';
+
+-- AlterEnum
+ALTER TYPE "UserRole" ADD VALUE 'superadmin';
