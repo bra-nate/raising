cfe02fa2dd9968b9575807ee24f050e268fe244f
Nate <pasiebu@gmail.com>
2026-07-07 22:46:27 +0000

    feat(scheduler): start cron jobs on server boot

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import { createApp } from './app';
 import { config } from './config';
+import { startScheduler } from './jobs/scheduler';
 
 const app = createApp();
 
 app.listen(config.port, () => {
   console.log(`raising API listening on http://localhost:${config.port} (${config.nodeEnv})`);
+  startScheduler();
 });
 
 export default app;
